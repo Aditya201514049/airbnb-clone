b0a4360bcd139cd65a9de6c3239e88572e3806c5
Aditya201514049 <adityasingha084@gmail.com>
2025-09-25 02:17:56 +0600

    implement MongoDB connection utility and add API route for testing connection

---
+++ app/api/test/route.js
@@ -0,0 +1,14 @@
+import { connectDB } from "@/lib/mongodb";
+
+export async function GET() {
+  try {
+    await connectDB();
+    return new Response(JSON.stringify({ message: "MongoDB connected!" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
